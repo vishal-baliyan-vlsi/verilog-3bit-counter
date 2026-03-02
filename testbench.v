304517701d545867c18f48d196b389dee54056fa
Vishal Baliyan <vishal.baliyan.vlsi@gmail.com>
2026-03-02 14:35:15 +0530

    Added structured testbench with clock, reset, and monitor

---
+++ testbench.v
@@ -0,0 +1,23 @@
+module counter_3bit_tb;
+reg clk;
+reg rst;
+wire [2:0] q;
+
+counter_3bit uut (
+    .clk(clk),
+    .rst(rst),
+    .q(q));
+
+initial begin
+   clk = 0;
+  forever #5 clk = ~clk;
+end
+  initial begin
+    rst = 1;
+    #10 rst = 0;
+    #50 $finish;
+end
+initial begin
+  $monitor ("%0t | rst = %b | q = %b", $time, rst, q);
+  end
+  endmodule
